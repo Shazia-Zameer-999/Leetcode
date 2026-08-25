9ac876d247496dfc14bba474466e9dabdecaba97
Shazia Zameer <datendiva7867@gmail.com>
2026-08-25 21:59:05 +0530

    Time: 1 ms (10.92%), Space: 19.3 MB (53.95%) - LeetHub

---
+++ 0551-student-attendance-record-i/0551-student-attendance-record-i.py
@@ -0,0 +1,19 @@
+class Solution:
+    def checkRecord(self, s: str) -> bool:
+        count_A=0
+        count_L=0
+        max_L=0
+        for i in s:
+            if i=="A":
+                count_A+=1
+            if i=="L":
+                count_L+=1
+            else:
+                count_L=0
+            if count_L>max_L:
+                max_L=count_L
+        if count_A>1 or max_L>2:
+            return False
+        else:
+            return True
+        
